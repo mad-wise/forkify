1a97a473ac0d87c421ed342ea1948d1138242df0
mad-wise <n.n.somova@gmail.com>
2022-11-09 16:02:15 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8d5290a8-8a79-4f91-b8e0-f2f6513a8548';
+export const MODAL_CLOSE_SEC = 2.5;
